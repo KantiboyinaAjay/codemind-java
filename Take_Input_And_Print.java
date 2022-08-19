171e95cf6a73491664cdd88cb555bce82155de48
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-08-19 10:49:30 +0530

    import java.util.*; class KCPD {     public static void main(String arg[])     {         Scanner scan = new Scanner(System.in);         String a = scan.nextLine();         System.out.print(a);         scan.close();     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class KCPD
+{
+    public static void main(String arg[])
+    {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.nextLine();
+        System.out.print(a);
+        scan.close();
+    }
+}
