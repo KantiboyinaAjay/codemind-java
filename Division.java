5c48888faf7834b20ba3a8aa265af67c0c50b937
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-08-19 10:38:57 +0530

    import java.util.*; class Kcpd {     public static void main(String asd[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         int b = scan.nextInt();         scan.close();         System.out.print(a/b);     } }

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Kcpd
+{
+    public static void main(String asd[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        scan.close();
+        System.out.print(a/b);
+    }
+}
