ebc5cf1fb7019b2d252437b204d4cdbe4621f3b0
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-08-19 10:31:40 +0530

    import java.util.*; class KCPD {     public static void main(String asd[])     {         Scanner scan = new Scanner(System.in);         double a = scan.nextDouble();         double b = scan.nextDouble();         scan.close();         System.out.format("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class KCPD
+{
+    public static void main(String asd[])
+    {
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        scan.close();
+        System.out.format("%.2f",a*b);
+    }
+}
