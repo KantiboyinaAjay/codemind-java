4a890a350d81ccf98450c3e07aa5b07953da8f91
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-08-19 10:34:56 +0530

    import java.util.*; class KCPD {     public static void main(String art[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         int b = scan.nextInt();         scan.close();         System.out.print(a*b);     } }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class KCPD
+{
+    public static void main(String art[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        scan.close();
+        System.out.print(a*b);
+    }
+}
