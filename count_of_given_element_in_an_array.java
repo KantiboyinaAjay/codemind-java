bda47e47ec574c7ac6fae4ee352432db1431ebb0
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-08-24 19:34:50 +0530

    import java.util.*; class average {     public static void main(String asd[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         int[] arr = new int[a];         for(int i=0;i<a;i++)         {             arr[i] = scan.nextInt();         }         int b = scan.nextInt();         scan.close();         int c=0;         for(int i=0;i<a;i++)         {             if(b==arr[i])             {                 c++;             }         }         System.out.print(c);     } }

---
+++ count_of_given_element_in_an_array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class average
+{
+    public static void main(String asd[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int[] arr = new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i] = scan.nextInt();
+        }
+        int b = scan.nextInt();
+        scan.close();
+        int c=0;
+        for(int i=0;i<a;i++)
+        {
+            if(b==arr[i])
+            {
+                c++;
+            }
+        }
+        System.out.print(c);
+    }
+}
