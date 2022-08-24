d7bc1f669c209db007975fcdfd474b70dbdd52dc
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-08-24 19:16:04 +0530

    import java.util.*; class average {     public static void main(String asd[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         int[] arr = new int[a];         for(int i=0;i<a;i++)         {             arr[i] = scan.nextInt();         }         scan.close();         int c=0;         for(int i=0;i<a;i++)         {             if(arr[i]%2==0)             {                 c++;             }         }         if(c==a)         {             System.out.print("True");         }         else         {             System.out.print("False");         }     } }

---
+++ Even_Array.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class average
+{
+    public static void main(String asd[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int[] arr = new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i] = scan.nextInt();
+        }
+        scan.close();
+        int c=0;
+        for(int i=0;i<a;i++)
+        {
+            if(arr[i]%2==0)
+            {
+                c++;
+            }
+        }
+        if(c==a)
+        {
+            System.out.print("True");
+        }
+        else
+        {
+            System.out.print("False");
+        }
+    }
+}
