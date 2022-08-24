c4b8891d66ccc9a637a70094c9c3be101a880883
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-08-24 11:12:13 +0530

    import java.util.*; class prime {     public static void main(String asd[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         int[] arr = new int[a];         for(int i=0;i<a;i++)         {             arr[i]=scan.nextInt();         }         int b = scan.nextInt();         int c = scan.nextInt();         scan.close();         int d=0,e=0;         for(int i=0;i<a;i++)         {             if(arr[i]>=b && arr[i]<=c)             {                 e+=arr[i];                 d=1;             }         }         if(d==0)         {             System.out.print("-1");         }         else         {             System.out.print(e);         }     } }

---
+++ sum_of_elements_between_A_and_B.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class prime
+{
+    public static void main(String asd[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int[] arr = new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i]=scan.nextInt();
+        }
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        int d=0,e=0;
+        for(int i=0;i<a;i++)
+        {
+            if(arr[i]>=b && arr[i]<=c)
+            {
+                e+=arr[i];
+                d=1;
+            }
+        }
+        if(d==0)
+        {
+            System.out.print("-1");
+        }
+        else
+        {
+            System.out.print(e);
+        }
+    }
+}
