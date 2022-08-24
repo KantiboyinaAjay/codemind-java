8188401294436713daef7be72f9cc17013a0997c
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-08-24 18:15:37 +0530

    import java.util.*; class sum {     public static void main(String asd[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         int[] arr = new int[a];         for(int i=0;i<a;i++)         {             arr[i] = scan.nextInt();         }         int b = scan.nextInt();         int c = scan.nextInt();         scan.close();         int d=0;         for(int i=0;i<a;i++)         {             if(arr[i]>=b && arr[i]<=c)             {                 continue;             }             else             {                 System.out.print(arr[i]+" ");                 d=1;             }         }         if(d==0)         {             System.out.print("-1");         }     } }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class sum
+{
+    public static void main(String asd[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int[] arr = new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i] = scan.nextInt();
+        }
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        int d=0;
+        for(int i=0;i<a;i++)
+        {
+            if(arr[i]>=b && arr[i]<=c)
+            {
+                continue;
+            }
+            else
+            {
+                System.out.print(arr[i]+" ");
+                d=1;
+            }
+        }
+        if(d==0)
+        {
+            System.out.print("-1");
+        }
+    }
+}
