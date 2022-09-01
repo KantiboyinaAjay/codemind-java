e21c2dddfe2a0412d06260e09f2c26ee85d292fe
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-09-01 22:12:31 +0530

    import java.util.*; class solutions {     public static void main(String asd[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         int n = (a-1)%9+1;         System.out.print(n);     } }

---
+++ Add_Digits.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class solutions
+{
+    public static void main(String asd[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int n = (a-1)%9+1;
+        System.out.print(n);
+    }
+}
