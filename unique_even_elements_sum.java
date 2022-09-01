1fa90706130515b9f4672826df84f1af8eda0cec
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-09-01 21:01:36 +0530

    import java.util.*; class solutions {     public static void main(String asd[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         int[] arr = new int[a];         for(int i=0;i<a;i++)         {             arr[i] = scan.nextInt();         }         int c=0;         for(int i=0;i<a;i++)         {             int n=0;             for(int j=i+1;j<a;j++)             {                 if(arr[i]==arr[j])                 {                     n++;                 }             }             if(n==0)             {                 if(arr[i]%2==0)                 {                     c+=arr[i];                 }             }         }         System.out.print(c);     } }

---
+++ unique_even_elements_sum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class solutions
+{
+    public static void main(String asd[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int[] arr = new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i] = scan.nextInt();
+        }
+        int c=0;
+        for(int i=0;i<a;i++)
+        {
+            int n=0;
+            for(int j=i+1;j<a;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    n++;
+                }
+            }
+            if(n==0)
+            {
+                if(arr[i]%2==0)
+                {
+                    c+=arr[i];
+                }
+            }
+        }
+        System.out.print(c);
+    }
+}
